3be10a643006235240dcfdafb6128ad88f224bfc
IsaacWP121 <47909717+IsaacWP121@users.noreply.github.com>
2021-10-20 08:00:09 +1100

    Starting on discord bot
    I have already got most of the api work done, currently creating discord.py bot

---
+++ main.py
@@ -0,0 +1,22 @@
+from smashGG import *
+import discord
+from discord.ext import commands
+from discord_components import DiscordComponents
+
+discordBot = commands.Bot(command_prefix = "&", self_bot=False, intents=discord.Intents.all()) #initializing discord client
+activity = discord.Activity(name="everyone lose their sets", type=discord.ActivityType.watching)
+
+
+
+@discordBot.event
+async def on_ready():
+    DiscordComponents(discordBot)
+    print("head2headbot is running")
+
+
+if __name__ == "__main__":
+    discordBot.run()
+    #client = smashGG(id=1572895) # either use the player id (made for testing purposes)
+    client = smashGG(slug="https://smash.gg/user/d648e3d5") # or put in a smash.gg profile link
+    #print(client.games)
+    print(client.h2hscores("CoffeeMuncher", game="Rushdown Revolt"))
---
+++ smashGG.py
@@ -0,0 +1,215 @@
+import requests, apitoken, time, difflib
+
+class smashGG:
+    def __init__(self, id=None, slug=None):
+        self.api = apitoken.token()
+        self.url = "https://api.smash.gg/gql/alpha"
+        self.id = id
+        try:
+            self.slug = slug.replace("https://", "").replace("smash.gg/user/", "")
+        except:
+            print("using id")
+
+        # defining queries
+        self.q_playersets = """
+                    query Sets($id:ID!) {
+                        player(id:$id) {
+                            id
+                            gamerTag
+                            sets(perPage: 500, page: 1) {
+                                nodes {
+                                    id
+                                    displayScore
+                                    event {
+                                        id
+                                        name
+                                        videogame {
+                                            name
+                                        }
+                                        tournament {
+                                            id
+                                            name
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                """
+        self.q_set_entrants = """
+                    query User($descrim: String!) {
+                        user(slug: $descrim) {
+                            player {
+                                gamerTag
+                                sets(perPage: 500, page: 1) {
+                                    nodes {
+                                        id
+                                        event {
+                                        videogame {
+                                            name
+                                        }
+                                        }
+                                        slots {
+                                            id
+                                            entrant {
+                                                participants {
+                                                    gamerTag
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },"""
+        self.q_playersetsBySlug = """
+                            query User($descrim: String!) {
+                                user(slug: $descrim) {
+                                    player {
+                                        id
+                                        gamerTag
+                                        sets(perPage: 500, page: 1) {
+                                            nodes {
+                                                id
+                                                displayScore
+                                                event {
+                                                    id
+                                                    name
+                                                    videogame{
+                                                        name
+                                                    }
+                                                    tournament {
+                                                        id
+                                                        name
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },"""
+        self.create_set_lists()  
+
+
+    def create_set_lists(self):
+        if self.id != None:
+            req = requests.post(
+                self.url, data = {
+                    "query": self.q_playersets, "variables": f'{{"id": {self.id}}}'
+                }, headers={'Authorization': f'Bearer {self.api}'}).json()
+            self.all_sets = req["data"]["player"]["sets"]["nodes"]
+            self.tag = req["data"]["player"]["gamerTag"]
+                # returns a list of dicts with some sort of structure like this{id, displayScore, event={id, name, tournament={id, name}}} (everything inside of the nodes part of the query)
+
+        elif self.slug != None:
+            req = requests.post(
+                self.url, data = {
+                    "query": self.q_playersetsBySlug, "variables": f'{{"descrim": "{self.slug}"}}'
+                }, headers={'Authorization': f'Bearer {self.api}'}).json()
+            self.all_sets = req["data"]["user"]["player"]["sets"]["nodes"]
+            self.tag = req["data"]["user"]["player"]["gamerTag"]
+
+        else:
+            print("Slug or id must be present")
+            return
+
+        self.sets_minusdqs = []
+        
+        for i in self.all_sets:
+            if i["displayScore"] != "DQ":
+                self.sets_minusdqs.append(i)
+        self.games = []
+        for i in range(len(self.sets_minusdqs)):
+            if not self.sets_minusdqs[i]["event"]["videogame"]["name"] in self.games:
+                self.games.append(self.sets_minusdqs[i]["event"]["videogame"]["name"])
+
+
+    def get_set_entrants(self):
+        self.set_entrants = [] # will get all the entrants in the list of the players sets
+
+        req = requests.post(
+            self.url,
+            data={
+                "query": self.q_set_entrants,
+                "variables": f'{{"descrim": "{self.slug}"}}',
+            },
+            headers={
+                'Authorization': f'Bearer {self.api}',
+            }
+        ).json()['data']["user"]["player"]['sets']["nodes"]
+
+        
+        for i in req:
+            _temp = []
+            for it in i["slots"]:
+                _temp.append([i["id"], it['entrant']["participants"][0]['gamerTag'], i["event"]["videogame"]["name"]])
+            self.set_entrants.append(_temp)
+        return self.set_entrants# arranged where x[?] returns a nested list containing the following information [["SetId", "Player1Tag", game], ["SetId", "Player2Tag", game]] 
+    
+
+    def create_head2heads(self, game):
+        Sets = self.get_set_entrants()
+        # orders things into a dict with the key of the players name, and the value of a list of ids
+        self.head2head = {}
+        if game == "all":
+            for i in Sets: # for every set in the list of sets
+                if i[0][1] == self.tag:
+                    if i[1][1] == self.tag:
+                        print("Error with head2heads")
+                        return
+                    else:
+                        if i[1][1] in self.head2head:
+                            self.head2head[i[1][1]].append(i[1][0])
+                        else:
+                            self.head2head[i[1][1]] = [i[1][0]]
+                else:
+                    if i[0][1] in self.head2head:
+                        self.head2head[i[0][1]].append(i[0][0])
+                    else:
+                        self.head2head[i[0][1]] = [i[1][0]]
+        else:
+            for i in Sets: # for every set in the list of sets
+                if i[0][2] == game:
+                    if i[0][1] == self.tag:
+                        if i[1][1] == self.tag:
+                            print("Error with head2heads")
+                            return
+                        else:
+                            if i[1][1] in self.head2head:
+                                self.head2head[i[1][1]].append(i[1][0])
+                            else:
+                                self.head2head[i[1][1]] = [i[1][0]]
+                    else:
+                        if i[0][1] in self.head2head:
+                            self.head2head[i[0][1]].append(i[0][0])
+                        else:
+                            self.head2head[i[0][1]] = [i[1][0]]
+        self.oppponents = list(self.head2head.keys())
+
+
+    def h2hscores(self, playerTag="all", game="all"): # needs to be a list of id's not a list of sets
+        self.create_head2heads(game)
+        self.scores = []
+        _temp = []
+        
+        for i in self.head2head:
+            _temp.append(i)
+
+        if playerTag != "all":
+            if playerTag in _temp:
+                _temp2 = difflib.get_close_matches(playerTag, _temp, n=3)
+                for i in self.head2head[_temp2[0]]:
+                    for it in self.sets_minusdqs:
+                        if it["id"] == int(i):
+                            self.scores.append(it["displayScore"])
+            else:
+                return(f"This user has not played against {playerTag}")
+
+        else:
+            for a in self.head2head:
+                for i in self.head2head[a]:
+                    print(i)
+                    for it in self.sets_minusdqs:
+                        if it["id"] == int(i):
+                            self.scores.append(it["displayScore"])
+        return self.scores
